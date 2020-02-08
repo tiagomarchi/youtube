850e99c89449afd6e961ceadb858daf2eb7ae643
enter <enter@enterapp.com.br>
2020-02-08 18:55:52 -0300

    Imports

--- WebAngular/youtube/src/app/app.component.ts
+++ WebAngular/youtube/src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { YoutubeService } from './services/youtube.service';
+import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -8,13 +9,24 @@ import { YoutubeService } from './services/youtube.service';
 })
 export class AppComponent {
   title = 'youtube';
+  customer : any;
+  form: FormGroup;
   items : Array<any> = new Array();
-  constructor(private _youtubeService : YoutubeService){
+  isLoading : boolean = false;
+
+  constructor(private _youtubeService : YoutubeService,private _formBuilder: FormBuilder){
+
+    this.form = this._formBuilder.group({
+      search:  ['', Validators.required],
+    })
 
     this._youtubeService.getSearch('wsl').subscribe((response: any) => {
       this.items = response;
     });
 
+  }
+  displayCustomerFn(customer: any) {
+    if (customer){ this.customer = customer; return customer.name; }
   }
 
 }
